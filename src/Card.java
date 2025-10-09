41f25816a468133a9d8f101b3b27f4dad7b1830a
apxt05 <xtp201@exeter.ac.uk>
2025-10-09 17:25:43 +0100

    card.java started

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,16 @@
-public class Card {
-    public static void main(String[] args) {
-        
+public final class Card {
+    private final int value;
+
+    //ensure the card is positive value
+    public Card(int value) {
+        if (value<0) {
+            throw new IllegalArgumentException("Card number must be a positive integer");
+        }
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
+
