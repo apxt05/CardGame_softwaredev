1d3b60bb296c04910935a9b648825efb5de73628
RumiMansoubi <113688649+RumiMansoubi@users.noreply.github.com>
2025-10-15 17:00:22 +0000

    Start of Tests

---
+++ src/Tests/Card_Test.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import com.ibm.decision.run.test.junit5.DecisionTest;
+import com.ibm.decision.run.test.junit5.JSONTestDirectoryFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.TestFactory;
+ 
+import java.util.stream.Stream;
+ 
+@DecisionTest(decisionFunction = "<functionID"> /* OR  decisionOperation = "<operationID>" */)
+@DisplayName("<DisplayName>")
+public class <TestClassName> {
+    @TestFactory
+    public Stream<DynamicNode> decisionTests() {
+        JSONTestDirectoryFactory.verbose = true;
+        return JSONTestDirectoryFactory.createTests(this.getClass());
+    }
+}
